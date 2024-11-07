0ec677027464e7c65cd834fd7fc7ad4da26f8ff7
ElisaMavilia <elisamavilia1@gmail.com>
2024-11-07 10:39:04 +0100

    iteration

---
+++ iteration.js
@@ -0,0 +1,9 @@
+'use strict'; 
+
+/* console.log('Lifting weights repetition 1');
+ */
+//for loop keeps runnig while the condition is TRUE
+for(let rep = 1; rep <= 10; rep ++){
+    console.log(`Lifting weights repetition ${rep}`); 
+};
+// rep ++ entails rep = rep +1;
